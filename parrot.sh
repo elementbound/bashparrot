02633501360bc1c16653b47d583b44f31e158849
Tamas Galffy <ezittgtx@gmail.com>
2018-09-28 16:28:59 +0200

    Party parrot

--- parrot.sh
+++ parrot.sh
@@ -10,13 +10,18 @@ function parrot() {
 
     clear
     frame=-1
+    color=-1
 
     while true; do
         frame=$(( (frame+1) % (frame_max+1) ))
+        color=$(( (color+1) % 6))
 
         tput cup 0 0 
+        tput setaf $((1 + color))
         echo "${frames[frame]}"
     done
+
+    tput setaf 7
 }
 
 parrot
